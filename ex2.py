5e5f217b5e61828d3f8b6fba8911b95578d9f387
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2023-07-01 14:54:41 +0800

    Add files via upload

--- ex2.py
+++ ex2.py
@@ -15,6 +15,7 @@ import albumentations as A
 import glob
 import os
 # study https://albumentations.ai/docs/getting_started/image_augmentation/
+#semantci augmentation : https://albumentations.ai/docs/getting_started/mask_augmentation/
 
 def visualize(image,count,type_):
     plt.figure(figsize=(150,90))
@@ -35,13 +36,14 @@ for img in img_list:
     transform = A.Compose([
         #A.Perspective(),
         #A.RandomCrop(360, 640),
+        A.UnsharpMask (blur_limit=(3, 7), sigma_limit=0.0, alpha=(0.2, 0.5), threshold=10, always_apply=True, p=0.5)
         #A.RandomGravel (gravel_roi=(0.1, 0.4, 0.9, 0.9), number_of_patches=2, always_apply=False, p=0.5)
         #A.ISONoise (color_shift=(0.01, 0.05), intensity=(0.1, 0.5), always_apply=True, p=0.5)
         #A.PixelDropout (dropout_prob=0.01, per_channel=False, drop_value=0, mask_drop_value=None, always_apply=True, p=0.5)
         #A.RandomBrightness (limit=0.7, always_apply=True, p=0.5)
         #A.RandomBrightnessContrast (brightness_limit=0.05, contrast_limit=0.05, brightness_by_max=False, always_apply=True, p=0.5) 
         #A.RandomGamma (gamma_limit=(80, 120), eps=None, always_apply=True, p=0.5)
-        A.RandomSunFlare (flare_roi=(0.5, 0.2, 0.6, 0.3), angle_lower=0, angle_upper=1, num_flare_circles_lower=6, num_flare_circles_upper=10, src_radius=450, src_color=(255, 255, 255), always_apply=True, p=0.5)
+        #A.RandomSunFlare (flare_roi=(0.5, 0.2, 0.6, 0.3), angle_lower=0, angle_upper=1, num_flare_circles_lower=6, num_flare_circles_upper=10, src_radius=450, src_color=(255, 255, 255), always_apply=True, p=0.5)
         #A.RandomSnow (snow_point_lower=0.1, snow_point_upper=0.3, brightness_coeff=2.5, always_apply=True, p=0.5)
         #A.RandomShadow (shadow_roi=(0, 0.5, 1, 1), num_shadows_lower=2, num_shadows_upper=4, shadow_dimension=7, always_apply=True, p=0.5)
         #A.RandomFog(fog_coef_lower=0.3, fog_coef_upper=1.0,alpha_coef=0.10, always_apply=True, p=0.5),
@@ -59,5 +61,5 @@ for img in img_list:
     random.seed(42)
     np.random.seed(42)
     transformed = transform(image=image)
-    visualize(transformed['image'],count,type_="sunflare")     
+    visualize(transformed['image'],count,type_="unsharp")     
     count+=1
